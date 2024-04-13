07edf25d433e3fc0c9727b73702eabf5975df5c0
bunturioto <Bunturioto@gmail.com>
2024-04-13 13:51:28 +0400

    Define interface for media player

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from abc import ABC, abstractmethod
 
+# Define interface for media player
+class MediaPlayer(ABC):
+    @abstractmethod
+    def pause(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    @abstractmethod
+    def rewind(self, seconds):
+        pass
+
+    @abstractmethod
+    def fast_forward(self, seconds):
+        pass
